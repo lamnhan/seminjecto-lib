6432f405204f762be97fee2e5f262ed768ca96c1
Lam Nhan <lamnhan92@gmail.com>
2019-12-20 17:28:26 +0700

    1.0.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
-import { Main } from './main';
 
-export function main() {
-  return new Main();
+export class Main {
+
+  constructor() {
+  }
+
 }
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,2 +1,1 @@
-export { Main as LibModule } from './lib/main';
-export { main as lib } from './lib/index';
+export { Main as LibModule } from './lib/index';
